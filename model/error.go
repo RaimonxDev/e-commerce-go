9a1637b1e606482a1d2428640480700056dc6fb2
Ramon Martinez <ramonmartinezcordero@gmail.com>
2022-12-12 12:00:54 -0300

    Change type string for error

--- model/error.go
+++ model/error.go
@@ -4,18 +4,21 @@ import "fmt"
 
 type Error struct {
 	Code       string
-	Err        string
+	Err        error
 	Who        string
 	StatusHTTP int
 	Data       interface{}
 	ApiMessage string
-	userID     string
+	UserID     string
 }
 
 func (e *Error) Error() string {
 	return fmt.Sprintf("Code: %s, Error: %s, Who:%s, StatusHttp:%d, Data: %v, UserID:%s",
-		e.Code, e.Err, e.Who, e.StatusHTTP, e.Data, e.userID)
+		e.Code, e.Err, e.Who, e.StatusHTTP, e.Data, e.UserID)
 
+}
+func NewError() Error {
+	return Error{}
 }
 
 func (e *Error) HasCode() bool {
@@ -25,6 +28,6 @@ func (e *Error) HasCode() bool {
 func (e *Error) HasStatusHttp() bool {
 	return e.StatusHTTP > 0
 }
-func (e *Error) hastData() bool {
+func (e *Error) HasData() bool {
 	return e.Data != nil
 }
