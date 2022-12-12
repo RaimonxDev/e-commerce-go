bb2aff9476e8a312f45b60b55e151a418e2adac6
Ramon Martinez <ramonmartinezcordero@gmail.com>
2022-12-12 12:02:02 -0300

    init app

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"github.com/RaimonxDev/e-commerce-go.git/infrastucture/handler/response"
+	"log"
+	"os"
+)
+
+func main() {
+
+	err := loadEnv()
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = validateEnvironments()
+	if err != nil {
+		log.Fatal(err)
+	}
+	e := newHTTP(response.HTTPErrorHandler)
+
+	db, err := newDbConnection()
+	if err != nil {
+		log.Fatal(err)
+	}
+	_ = db
+	err = e.Start(":" + os.Getenv("SERVER_PORT"))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
