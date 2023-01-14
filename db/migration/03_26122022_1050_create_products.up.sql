1a374ac10459abe678311d035bddc8451f181588
Ramon Martinez <ramonmartinezcordero@gmail.com>
2023-01-14 09:20:10 -0300

    add sql migrations

--- db/migration/03_26122022_1050_create_products.up.sql
+++ db/migration/03_26122022_1050_create_products.up.sql
@@ -2,10 +2,11 @@ CREATE TABLE IF NOT EXISTS products(
     id UUID NOT NULL,
     name VARCHAR(128) NOT NULL,
     price NUMERIC(10) NOT NULL,
+	details JSONB NOT NULL,
     images JSONB NOT NULL,
     description TEXT NOT NULL,
     features JSONB NOT NULL,
-    created_at INTEGER NOT NULL DEFAULT EXTRACT(EPOCH FROM NOW())::INTEGER,
+    created_at INTEGER NOT NULL DEFAULT EXTRACT(EPOCH FROM NOW())::INT,
     updated_at INTEGER,
     CONSTRAINT products_id_pk PRIMARY KEY (id)
 )
